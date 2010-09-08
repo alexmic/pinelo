eed9e39cbef4fd3f7d997720e5efe41ae05dbc43
Alex Michael <am607@imperial.ac.uk>
2010-09-08 05:39:57 -0700

    FIX: loop in arc brush.

--- pinelo/src/brushes/arc.js
+++ pinelo/src/brushes/arc.js
@@ -15,7 +15,7 @@ palette.extend({
                  var prev = this.points[p_l - 1];
                  ctx.strokeStyle = 'rgba(0,0,0,0.3)';
                  ctx.lineWidth = 0.1;
-                 for (var i = p_l - 1 ; i--;) {
+                 for (var i = p_l ; i--;) {
                      var p = this.points[i];
                      var d = Util.math.d(x, p,x, y, p,y);
                      if (d < 35) {
