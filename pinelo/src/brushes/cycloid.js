dd1696025052d59fe514692c2af28af29fb4a789
Benjamin van Enckevort <benvan+git@gmail.com>
2010-09-17 22:45:37 +0200

    Converted remaining brushes

--- pinelo/src/brushes/cycloid.js
+++ pinelo/src/brushes/cycloid.js
@@ -3,38 +3,26 @@
  *   Author: Alex Michael
  */
 
-palette.extend({
-	
-	cycloid: {
-        prev: null,
-        angle: 0,
-        stroke: function(x, y) {
-            ctx.lineWidth = 5 * rnd();
-            ctx.strokeStyle = "rgba(0,0,0,0.8)";
-            var p = this.prev;
-            if (p) {
-                this.angle = (this.angle + 0.40) % (2 * M.PI);
-                var r  = 15;
-                x += r * cos(this.angle);
-                y += r * sin(this.angle);
-                Util.draw.line(p.x, x, p.y, y);
-            }
-
-            this.prev = {
-                x: x,
-                y: y
-            };
-        },
-
-        onMouseUp: function(e) {
-            this.prev = null;
-        },
-        
-        unload: function(e){
-        	this._super.unload();
-        	this.prev = null;
-        	this.angle = 0;
+palette.createBrush(Brush.extend({
+	name: 'cycloid',	
+    angle: 0,
+    stroke: function(layer, x, y) {
+        layer.ctx.lineWidth = 5 * rnd();
+        layer.ctx.strokeStyle = "rgba(0,0,0,0.8)";
+        var p = this.previous;
+        if (p) {
+        	var r  = 25;
+        	p.x += r * cos(this.angle);
+        	p.y += r * sin(this.angle);
+            this.angle = (this.angle + 0.40) % (2 * M.PI);
+           	x += r * cos(this.angle);
+            y += r * sin(this.angle);
+            layer.line(p.x, x, p.y, y);
         }
-	}
-	
-});
+    },
+
+    unload: function(e){
+    	this._super();
+    	this.angle = 0;
+    }
+}));
--- pinelo/src/brushes/flowers.js
+++ pinelo/src/brushes/flowers.js
@@ -3,73 +3,56 @@
  *   Author: Alex Michael
  */
  
-palette.extend({
-
-	flowers:{
-		prev: null,
-        stroke: function(x, y) {
-            
-            ctx.lineWidth = 3;
-            ctx.strokeStyle = "rgba(0,0,0,1)";
-            ctx.fillStyle = "rgba(0,0,0,1)";
-            
-            var p = this.prev;
-            if (p) {
+palette.createBrush(Brush.extend({
+	name: 'flowers',
+    stroke: function(layer, x, y) {
+        var ctx = layer.ctx;
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = "rgba(0,0,0,1)";
+        ctx.fillStyle = "rgba(0,0,0,1)";
+        
+        var p = this.previous;
+        if (p) {
+            var d = M.sqrt(M.pow(x - p.x, 2),
+                           M.pow(y - p.y, 2));
+            if (rnd() > Math.min((0.65 + 1 / d), 0.97)){
+                var xx = -400,
+                    anticlockwise = true;
+                    startAngle = 0,
+                    stopAngle  = -0.5,
+                    xxx = -47,
+                    yyy = -190;
+                if (rnd() > 0.5){
+                    xx = 400;
+                    anticlockwise = false;
+                    startAngle = M.PI;
+                    stopAngle  = M.PI + 0.5;
+                    xxx = 47;
+                }
                 
-                var d = M.sqrt(M.pow(x - p.x, 2),
-                               M.pow(y - p.y, 2));
-                if (rnd() > Math.min((0.65 + 1 / d), 0.97)){
-                    var xx = -400,
-                        anticlockwise = true;
-                        startAngle = 0,
-                        stopAngle  = -0.5,
-                        xxx = -47,
-                        yyy = -190;
-                    if (rnd() > 0.5){
-                        xx = 400;
-                        anticlockwise = false;
-                        startAngle = M.PI;
-                        stopAngle  = M.PI + 0.5;
-                        xxx = 47;
-                    }
-                    
+                ctx.beginPath();
+                ctx.arc(x + xx, y, 400, 
+                        startAngle, 
+                        stopAngle, 
+                        anticlockwise);
+                ctx.stroke();
+                ctx.beginPath();
+                var nx = x + xxx,
+                    ny = y + yyy;
+                ctx.arc(nx, ny, 7, 0, M.PI * 2, true);
+                ctx.fill();
+                ctx.fillStyle = "rgba(255,0,255,1)";
+                for (var i = 0; i < 5; i++){
                     ctx.beginPath();
-                    ctx.arc(x + xx, y, 400, 
-                            startAngle, 
-                            stopAngle, 
-                            anticlockwise);
-                    ctx.stroke();
-                    ctx.beginPath();
-                    var nx = x + xxx,
-                        ny = y + yyy;
-                    ctx.arc(nx, ny, 7, 0, M.PI * 2, true);
-                    ctx.fill();
-                    ctx.fillStyle = "rgba(255,0,255,1)";
-                    for (var i = 0; i < 5; i++){
-                        ctx.beginPath();
-                        var a  = i * ((72 * 2 * M.PI) / 360); 
-                        var dx = 11 * cos(a);
-                        var dy = 11 * sin(a);
-                        ctx.arc(nx + dx, 
-                                ny + dy, 8, 0, M.PI * 2, true);
-                        ctx.fill();  
-                    }
+                    var a  = i * ((72 * 2 * M.PI) / 360); 
+                    var dx = 11 * cos(a);
+                    var dy = 11 * sin(a);
+                    ctx.arc(nx + dx, 
+                            ny + dy, 8, 0, M.PI * 2, true);
+                    ctx.fill();  
                 }
             }
-
-            this.prev = {
-                x: x,
-                y: y
-            };
-        },
-
-        onMouseUp: function(e) {
-            this.prev = null;
-        },
-        
-        unload: function(e){
-        	this._super.unload();
-        	this.prev = null;
         }
-	}
-});
+
+    }
+}));
